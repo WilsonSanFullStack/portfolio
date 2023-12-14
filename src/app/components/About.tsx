c1d0b8be81d6b62e11cef1913d5f7b68ff190160
WilsonSanFullStack <clinicasystemlab@gmail.com>
2023-12-14 08:26:48 -0500

    creacion del abaut

---
+++ src/app/components/About.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="m-4">
+      <h1 className="text-center my-2 text-2xl font-bold">Sobre Mi</h1>
+      <p>
+        EL 01/08/2023 me gradue de HENRY como Programador web, desde entonces he
+        participado en varios proyectos con los cuales he optenido experiencia.
+      </p>
+      <p>
+        Soy una persona dedicada y comprometida me gusta leer, Jugar Ajedrez y
+        en la programacion he encontrado una forma de hacer lo que mas me gusta
+        crear cosas siento que programando puedo crear cosas muy agradables y
+        funcionales lo cual siempre fue mi deseo.
+      </p>
+      <p>
+        Me considero de rapido aprendizaje ademas me gusta siempre estar
+        aprendiendo cosas nueva y claro perfeccionando los conocimientos antes
+        adquiridos.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/app/components/Proyect.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Proyect = () => {
+  return (
+    <div className='m-4'>
+      <h1 className='uppercase my-2 text-center font-bold text-2xl'>Proyectos</h1>
+    </div>
+  )
+}
+
+export default Proyect
