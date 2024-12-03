4e43a282ee27a78ea1c728b0d9f3e99f8f3dafb0
WilsonSanFullStack <harveysanch@gmail.com>
2024-12-03 02:25:12 -0500

    se mejora el about me

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -5,19 +5,18 @@ const About = () => {
     <div className="m-4">
       <h1 className="text-center my-2 text-2xl font-bold">Sobre Mi</h1>
       <p>
-        EL 01/08/2023 me gradúe de HENRY como Programador web, desde entonces he
-        participado en varios proyectos con los cuales he obtenido experiencia.
-      </p>
-      <p>
-        Soy una persona dedicada y comprometida me gusta leer, Jugar Ajedrez y
-        en la programación he encontrado una forma de hacer lo que más me gusta
-        crear cosas siento que programando puedo crear cosas muy agradables y
-        funcionales lo cual siempre fue mi deseo.
-      </p>
-      <p>
-        Me considero de rápido aprendizaje además me gusta siempre estar
-        aprendiendo cosas nuevas y claro perfeccionando los conocimientos antes
-        adquiridos.
+        ¡Hola! Soy Wilson Sanchez, un desarrollador Full Stack con un gran
+        entusiasmo por construir experiencias digitales que impacten
+        positivamente. Desde interfaces que cautivan a usuarios hasta sistemas
+        robustos en el backend, me gusta abordar cada proyecto con creatividad,
+        detalle y pasión por el código. He trabajado en proyectos increíbles que
+        van desde plataformas colaborativas hasta aplicaciones interactivas en
+        3D, utilizando tecnologías como React.js, Node.js, TypeScript y más.
+        Para mí, cada línea de código es una oportunidad para aprender, mejorar
+        y aportar valor. Más allá de mi pasión por la tecnología, disfruto
+        colaborar con equipos de todo el mundo y aportar soluciones que
+        realmente marquen la diferencia. Si buscas innovación, dedicación y un
+        toque de creatividad en tus proyectos, estás en el lugar correcto.
       </p>
     </div>
   );
